88114d204012e7a13fa4ac8331942e348f32cfcc
vinicius bernardo <vbs4@cesar.school>
2024-11-08 19:04:30 -0300

    a funcao printFase1 imprime as bordas da fase1 mas nao os detalhes da fase

--- src/fases.c
+++ src/fases.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 
-#define LINHAS  24
-#define COLUNAS  100
+#define MIN_Y  0
+#define MAX_Y  24
+#define MIN_X  0
+#define MAX_X  100
+
 
 void printFase1(){
-    char matriz1[LINHAS][COLUNAS];
-    for(int i = 0; i < LINHAS; i++){
-        for(int j = 0; j < COLUNAS; j++){
-            printf("#");
+    char matriz1[MAX_Y][MAX_X];
+
+    for(int i = MIN_Y; i < MAX_Y; i++){
+        for(int j = MIN_X; j < MAX_X; j++){
+            if(i == 0){
+                matriz1[i][j] = '#';
+                printf("%c", matriz1[i][j]);
+            } else if(i == 23){
+                matriz1[i][j] = '#';
+                printf("%c", matriz1[i][j]);
+            } else if(j == 0){
+                 matriz1[i][j] = '#';
+                printf("%c", matriz1[i][j]);
+            } else if(j == 99){
+                 matriz1[i][j] = '#';
+                printf("%c", matriz1[i][j]);
+            } else {
+                 matriz1[i][j] = ' ';
+                printf("%c", matriz1[i][j]);
+            }
         }
         printf("\n");
     }
+
 }
 
 int main(){
